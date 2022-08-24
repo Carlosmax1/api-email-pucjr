0a49055eb31889f4798b48be68e6222e5156673c
Carlosmax1 <carlosjogosbr@gmail.com>
2022-08-24 13:19:42 -0300

    headers

--- src/server.js
+++ src/server.js
@@ -9,7 +9,15 @@ const port = process.env.PORT;
 
 app.use((res, req, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
   next();
 })
 
