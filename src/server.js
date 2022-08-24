110f37920d6ea4bbd5cf2ac3fb74319ef0bc24e5
Carlosmax1 <carlosjogosbr@gmail.com>
2022-08-24 16:11:11 -0300

    config

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://pucjunior.infoalto.com.br'
+}));
 app.use(routes);
 
 app.listen(port, () => {
